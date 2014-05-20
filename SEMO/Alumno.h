667ff14a8f51877d39fa102ca82370e0679cb3e6
PDM-115 <pdm-115@PDM-115s-Mac.local>
2014-05-20 16:15:47 -0600

    agregado alumno.h

---
+++ SEMO/Alumno.h
@@ -0,0 +1,17 @@
+//
+//  Alumno.h
+//  SEMO
+//
+//  Created by PDM-115 on 20/05/14.
+//  Copyright (c) 2014 Grupo19. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Alumno : NSObject
+@property (nonatomic,strong)NSString *carnet;
+@property (nonatomic,strong)NSString *username;
+@property (nonatomic,strong)NSString *nombre;
+@property (nonatomic,strong)NSString *apellido;
+
+@end
